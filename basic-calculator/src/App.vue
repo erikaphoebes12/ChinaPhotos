e7f575658e32e93fc2c574c11d91b1152a686605
erikaphoebes12@gmail.com <erikaphoebes12@gmail.com>
2021-02-14 00:37:48 +0800

    Basic Calculator
    with Simple and Modern Arrangement of buttons using Vue 3.

---
+++ basic-calculator/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <simple-calculator msg="Simple Calculator"/>
+    <modern-calculator msg="Modern Calculator"/>
+  </div>
+</template>
+
+<script>
+import SimpleCalculator from './components/SimpleCalculator.vue'
+import ModernCalculator from './components/ModernCalculator.vue'
+
+export default {
+  name: 'App',
+  components: {
+    SimpleCalculator, 
+    ModernCalculator
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
